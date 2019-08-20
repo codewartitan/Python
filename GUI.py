2a4eb400e8e1c1ca506e81e219b3670e7f0d08ae
codewartitan <codewartitan@gmail.com>
2019-08-20 15:22:07 +0530

    this is the first the GUI application

---
+++ GUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# if you are still working under a Python 2 version, 
+# comment out the previous line and uncomment the following line
+# import Tkinter as tk
+
+root = tk.Tk()
+w = tk.Label(root, text="Hello Tkinter!")
+#w.pack()
+root.mainloop()
