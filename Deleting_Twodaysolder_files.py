1b388c3143db0ed66acbafa7e4c578ee02f730ce
codewartitan <46243777+codewartitan@users.noreply.github.com>
2019-10-01 18:01:14 +0530

    Update Deleting_Twodaysolder_files.py
    this will delete all the files which are older than two days

--- Deleting_Twodaysolder_files.py
+++ Deleting_Twodaysolder_files.py
@@ -1,12 +1,13 @@
 import os
 import time
 
-path = "C:/Users/tibbu/Documents"
+path = "C:/Users/tibbu/Documents/Agendas"
 now = time.time()
 dirs = os.listdir(path)
 for filename in os.listdir(path):
     # if os.stat(os.path.join(path, filename)).st_mtime < now - 7 * 86400:
-    if os.path.getmtime(os.path.join(path, filename)) < now - 7 * 86400:
+    if os.path.getmtime(os.path.join(path, filename)) < now - 2 * 86400:
         if os.path.isfile(os.path.join(path, filename)):
             print(filename)
             os.remove(os.path.join(path, filename))
+
