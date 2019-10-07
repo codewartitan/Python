515809126b86aa84f9c546dce6c6f387f7fd0136
codewar <codewartitan@gmail.com>
2019-10-07 16:32:18 +0530

    execrise

---
+++ Product.py
@@ -0,0 +1,9 @@
+# If the product is greater than 1000, then return their sum
+
+Num1 = int(input("Enter First Value: "))
+Num2 = int(input("Enter Second Value: "))
+product = Num1*Num2
+if product>1000:
+    print(Num1+Num2)
+else:
+    print(product)
