8b0f72b7634a8e8bb12b574cc25de6d4274c9c5b
codewartitan <codewartitan@gmail.com>
2020-01-05 19:13:30 +0530

    Enter the url to download the videos

--- DownLoad_GUI.py
+++ DownLoad_GUI.py
@@ -1,15 +1,21 @@
 import tkinter
-
+from pytube import YouTube
 root = tkinter.Tk()
 # Canvas is used to created a window
 canvas1 = tkinter.Canvas(root, width=400, height=300)
 Entry = tkinter.Entry(root)
 canvas1.create_window(200, 140, window=Entry)
 
+title_label = tkinter.Label(text="Calculate The Square Root")
+canvas1.create_window(200, 50, window=title_label)
+
 
 def getSquareRoot():
     x1 = Entry.get()
-    label1 = tkinter.Label(root, text=float(x1) ** 0.5)
+    label1 = tkinter.Label(root, text="Download Completed")
+    yt = YouTube(x1)
+    yt = yt.streams.first()
+    yt.download('/home/codewar/Videos/Down_Vidoes')
     canvas1.create_window(200, 230, window=label1)
 
 
