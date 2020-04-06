6e98b9850c1a4534f10a240831d9906d9dee7d75
codewartitan <codewartitan@gamil.com>
2020-04-06 22:43:18 +0530

    reading from csv file,doing google search & storing the results back to csv

--- GoogleSearchResult.py
+++ GoogleSearchResult.py
@@ -1,5 +1,26 @@
 from googlesearch import search
-dataset='planning Commission'+'CHEROKEE'+'ga'+'US'
+import pandas as pd
 
-for url in search(dataset, tld='com', stop=5):
-    print(url)
+Names = []  # List to store name of the product
+company = []  # List to store price of the product
+Links = []  # # List to store links
+df = pd.read_csv('listingsites.csv')
+names = df['Name']
+companyNames = df['company']
+
+# print(companyNames)
+
+# install google libaray for googleSearch to work
+
+# dataset = 'planning Commission' + 'CHEROKEE' + 'ga' + 'US'
+#
+#
+for names in companyNames:
+    for url in search(names, tld='com', stop=1):
+        # print(url)
+        Links.append(url)
+        # Names.append(df['Name'])
+        # company.append(df['company'])
+
+df = pd.DataFrame({'Name': df['Name'], 'company': df['company'], 'Links': Links})
+df.to_csv('CompanyLinks.csv', index=False, encoding='utf-8')
